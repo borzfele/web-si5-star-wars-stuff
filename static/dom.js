0ad6d1b9e82d56feab706ae61382cd1483646b19
borzfele <borzfele@gmail.com>
2017-07-10 16:31:57 +0200

    modal added

--- static/dom.js
+++ static/dom.js
@@ -8,7 +8,7 @@ app.dom = {
         $("#planets_data").html('');
         for (i = 0; i < datasToDisplay['results'].length; i++) {
             var defaultRow = document.getElementById('planet_data_x');
-            var clonedRow = defaultRow; 2
+            var clonedRow = defaultRow;
             clonedRow = clonedRow.outerHTML.replace(/x/g, i);
             $("#planets_data").append(clonedRow);
             $("#name_" + i).html(datasToDisplay['results'][i]['name']);
@@ -18,9 +18,19 @@ app.dom = {
             $("#surface_" + i).html(datasToDisplay['results'][i]['surface_water']);
             $("#population_" + i).html(datasToDisplay['results'][i]['population']);
             if (datasToDisplay['results'][i]['residents'].length === 1) {
-                $("#residents_" + i).html(datasToDisplay['results'][i]['residents'].length + " resident");
+                var defaultResidentsButton = document.getElementById('residents_button_x');
+                var clonedResidentsButton = defaultResidentsButton;
+                clonedResidentsButton = clonedResidentsButton.outerHTML.replace(/x/g, i);
+                $("#residents_" + i).html(clonedResidentsButton);
+                $("#residents_" + i).css("display", "block");
+                $("#residents_button_" + i).html(datasToDisplay['results'][i]['residents'].length + " resident");
             } else if (datasToDisplay['results'][i]['residents'].length > 1) {
-                $("#residents_" + i).html(datasToDisplay['results'][i]['residents'].length + " residents" );
+                var defaultResidentsButton = document.getElementById('residents_button_x');
+                var clonedResidentsButton = defaultResidentsButton;
+                clonedResidentsButton = clonedResidentsButton.outerHTML.replace(/x/g, i);
+                $("#residents_" + i).html(clonedResidentsButton);
+                $("#residents_" + i).css("display", "block");
+                $("#residents_button_" + i).html(datasToDisplay['results'][i]['residents'].length + " residents" );
             } else {
                 $("#residents_" + i).html("No residents" );
             };
--- static/main.js
+++ static/main.js
@@ -2,6 +2,16 @@ var app = app || {};
 
 app.init = function() {
     app.dataHandler.getPlanetsData(app.dataHandler.defUrl);
-}
 
+    $(".resident").on("click", function() {
+        $("#myModal").css("display", "block");
+    });
+    $(".close").on("click", function() {
+        $("#myModal").css("display", "none");
+    });
+    $("#myModal").on("click", function() {
+        $("#myModal").css("display", "none");
+    });
+};
+    
 app.init();
