25f0a11c4932a1af5779038fcba11dba324c919f
borzfele <borzfele@gmail.com>
2017-07-12 20:10:56 +0200

    modal data display

--- static/dataHandler.js
+++ static/dataHandler.js
@@ -2,14 +2,17 @@ var app = app || {};
 
 app.dataHandler = {
     defUrl: "http://swapi.co/api/planets/?page=1",
+    planets : [],
     getPlanetsData : function(url) {
         $.getJSON(url, function(response) {
             app.dom.generateTable(response);
         });
     },
-    getResidentsData : function(url) {
-        $.getJSON(url, function(response) {
-            app.dom.generateModalTable(response);
-        });
+    getResidentsData : function(urlList) {
+        for (i = 0; i < urlList.length; i++) {
+            $.getJSON(urlList[i], function(response) {
+                app.dom.generateModalTable(response);
+            });
+        }
     }
 }
--- static/dom.js
+++ static/dom.js
@@ -36,9 +36,11 @@ app.dom = {
             };
         }
         $(".resident").on("click", function() {
+            $("#modal_table_body").html('');
             $("#myModal").css("display", "block");
             residentId = event.target.id.slice(-1);
-            loadResidentsData(datasToDisplay['results'][residentId]['residents']);
+            argsList = datasToDisplay['results'][residentId]['residents'];
+            app.dataHandler.getResidentsData(argsList);
         });
         $(".close").on("click", function() {
             $("#myModal").css("display", "none");
@@ -48,6 +50,7 @@ app.dom = {
         });
         $("#planets_table").css('display', 'block');
         $("#planets_table").css('margin', 'auto');
+
         $("#next").on("click", function() {
                 var next = datasToDisplay['next'];
                 app.dataHandler.getPlanetsData(next);
@@ -61,7 +64,19 @@ app.dom = {
             }
         });
     },
-    generateModalTable : function(datasToDisplay) {
-
+    generateModalTable : function(residentsData) {
+        var defaultRow = document.getElementById('residents_data_x');
+        var clonedRow = defaultRow;
+        var rowsNum = $("#modal_table_body").children().length;
+        clonedRow = clonedRow.outerHTML.replace(/x/g, rowsNum);
+        $("#modal_table_body").append(clonedRow);
+        $("#residents_name_" + rowsNum).html(residentsData['name']);
+        $("#residents_height_" + rowsNum).html(residentsData['height']);
+        $("#residents_mass_" + rowsNum).html(residentsData['mass']);
+        $("#residents_skin_color_" + rowsNum).html(residentsData['skin_color']);
+        $("#residents_hair_color_" + rowsNum).html(residentsData['hair_color']);
+        $("#residents_eye_color_" + rowsNum).html(residentsData['eye_color']);
+        $("#residents_birth_year_" + rowsNum).html(residentsData['birth_year']);
+        $("#residents_gender_" + rowsNum).html(residentsData['gender']);
     }
 }
